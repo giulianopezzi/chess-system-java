99014451039c3419d64908a1f5eec7fd86a7028d
Giuliano F Pezzi <gfpezzi@gmail.com>
2020-01-31 07:53:46 -0300

    Camada xadrez e imprimir tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -23,5 +23,11 @@ public class Board {
 		this.columns = columns;
 	}
 
+	public Piece piece(int rows, int columns) {
+		return pieces[rows][columns];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
